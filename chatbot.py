7c418fa2cbf90d911084b24acdaa24629c069b1c
Byung Chun Kim <wizardbc@gmail.com>
2023-10-10 22:17:42 +0900

    Chat Bot

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import openai
 
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+
+# OpenAI API key
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -12,6 +16,18 @@ for msg in st.session_state.messages:
   if content := msg.get("content", ""):
     with st.chat_message(msg.get("role")):
       st.write(content)
+
+# ChatCompletion parameters
+chat_params = {
+  "model": "gpt-3.5-turbo-0613",
+  "n": 1,
+  "temperature": 1.0,
+  "max_tokens": 512,
+  "top_p": 1.0,
+  "presence_penalty": 0.0,
+  "frequency_penalty": 0.0,
+  "stream": False,
+}
 
 # Chat input
 if prompt := st.chat_input("What is up?"):
@@ -26,11 +42,13 @@ if prompt := st.chat_input("What is up?"):
   # Append to history
   st.session_state.messages.append(user_msg)
 
+  # ChatCompletion
+  response = openai.ChatCompletion.create(
+    messages=st.session_state.messages,
+    **chat_params
+  )
   # Assistant message
-  assistant_msg = {
-    "role": "assistant",
-    "content": f"Echo: {prompt}"
-  }
+  assistant_msg = dict(response.choices[0].message)
   # Display assistant message
   with st.chat_message("assistant"):
     st.write(assistant_msg.get("content"))
