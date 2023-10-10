ecd24703339d4690d3bb4958fc028f76fc95e8e4
Byung Chun Kim <wizardbc@gmail.com>
2023-10-10 22:30:13 +0900

    Side Bar

--- chatbot.py
+++ chatbot.py
@@ -17,17 +17,21 @@ for msg in st.session_state.messages:
     with st.chat_message(msg.get("role")):
       st.write(content)
 
-# ChatCompletion parameters
-chat_params = {
-  "model": "gpt-3.5-turbo-0613",
-  "n": 1,
-  "temperature": 1.0,
-  "max_tokens": 512,
-  "top_p": 1.0,
-  "presence_penalty": 0.0,
-  "frequency_penalty": 0.0,
-  "stream": False,
-}
+# Sidebar for parameters
+with st.sidebar:
+  # ChatCompletion parameters
+  st.header("Parameters")
+  chat_params = {
+    "model": st.selectbox("model", ["gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k-0613", "gpt-4-0613", "gpt-4-32k-0613"]),
+    "n": 1,
+    # "n": st.number_input("n", min_value=1, value=1),
+    "temperature": st.slider("temperature", min_value=0.0, max_value=2.0, value=1.0),
+    "max_tokens": st.number_input("max_tokens", min_value=1, value=512),
+    "top_p": st.slider("top_p", min_value=0.0, max_value=1.0, value=1.0),
+    "presence_penalty": st.slider("presence_penalty", min_value=-2.0, max_value=2.0, value=0.0),
+    "frequency_penalty": st.slider("frequency_penalty", min_value=-2.0, max_value=2.0, value=0.0),
+    "stream": False,
+  }
 
 # Chat input
 if prompt := st.chat_input("What is up?"):
