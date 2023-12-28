d8b398609da1271079a6e4d2905579951e750b4d
Byung Chun Kim <wizardbc@gmail.com>
2023-12-28 04:57:04 +0000

    Role selection

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ChatGPT Playground
-Put your OpenAI API Key in `.streamlit/secrets.toml`.
+Put your Google API Key in `.streamlit/secrets.toml`.
 ```config
-OPENAI_API_KEY = "YOUR_API_KEY"
+GOOGLE_API_KEY = "YOUR_API_KEY"
 ```
 Run streamlit app.
 ```bash
 pip install -r requirements.txt
 streamlit run chatbot.py
 ```
-Dependency: `openai`, `streamlit`.
+Dependency: `google-generativeai`, `streamlit`.
--- chatbot.py
+++ chatbot.py
@@ -12,9 +12,20 @@ if "api_key" not in st.session_state:
     st.session_state.api_key = ""
     st.write("Your Google API Key is not provided in `.streamlit/secrets.toml`, but you can input one in the sidebar for temporary use.")
 
+def init_messages() -> None:
+  st.session_state.messages = []
+
+def undo() -> None:
+  st.session_state.messages.pop()
+
+def set_generate(state=True):
+  st.session_state.generate = state
+
 # Initialize chat history
 if "messages" not in st.session_state:
-  st.session_state.messages = []
+  init_messages()
+if "generate" not in st.session_state:
+  set_generate(False)
 
 safety_settings={
   'harassment':'block_none',
@@ -38,10 +49,21 @@ with st.sidebar:
   else:
     genai.configure(api_key=st.session_state.api_key)
 
+  # Role selection and Undo
+  st.header("Chat")
+  chat_role = st.selectbox("role", ["user", "model"], index=0)
+  columns = st.columns([1,1,1])
+  with columns[0]:
+    st.button("Run", on_click=set_generate, type='primary', use_container_width=True)
+  with columns[1]:
+    st.button("Undo", on_click=undo, use_container_width=True)
+  with columns[2]:
+    st.button("Clear", on_click=init_messages, use_container_width=True)
+
   # ChatCompletion parameters
   st.header("Parameters")
   model_name = st.selectbox("model_name",
-      [m.name for m in genai.list_models() if 'generateContent' in m.supported_generation_methods])
+      ['gemini-pro', 'gemini-pro-vision'])
   generation_config = {
     "temperature": st.slider("temperature", min_value=0.0, max_value=1.0, value=0.9),
     "max_output_tokens": st.number_input("max_tokens", min_value=1, value=2048),
@@ -53,21 +75,27 @@ with st.sidebar:
 if prompt := st.chat_input("What is up?"):
   # User message
   user_msg = {
-    "role": "user",
+    "role": chat_role,
     "parts": [prompt],
   }
   # Display user message
-  with st.chat_message("user"):
+  with st.chat_message(chat_role):
     st.write(prompt)
   # Append to history
   st.session_state.messages.append(user_msg)
 
+  if chat_role == 'user':
+    set_generate()
+
+if st.session_state.generate:
+  set_generate(False)
   # Generate
   model = genai.GenerativeModel(model_name=model_name,
                                 generation_config=generation_config,
                                 safety_settings=safety_settings)
   response = model.generate_content(st.session_state.messages, stream=True)
 
+  # Stream display
   with st.chat_message("model"):
     placeholder = st.empty()
   text = ''
@@ -75,6 +103,7 @@ if prompt := st.chat_input("What is up?"):
     text += chunk.text
     placeholder.write(text + "▌")
   placeholder.write(text)
+  # Save the answer
   st.session_state.messages.append({
     'role': 'model',
     'parts': [text]
